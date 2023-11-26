9c253de7677faa5053b2773a2e29fff98fe20643
Sleepybandit <toga62@gmail.com>
2023-11-26 12:30:25 -0800

    Update install.sh
    Adding instructions for DDNS

--- setup/install.sh
+++ setup/install.sh
@@ -29,6 +29,9 @@ echo "===== 4. INSTALLING AWS SERVICES AND LINUX KERNEL PATCHING ====="
 # source /aws-foundry-ssl/setup/aws_hosted_zone_id.sh
 source /aws-foundry-ssl/setup/aws_linux_updates.sh
 
+# Attempt DDNS Setup
+source /aws-foundry-ssl/setup/record_set.sh
+
 # Set up TLS certificates with LetsEncrypt
 echo "===== 5. INSTALLING LETSENCRYPT CERTBOT ====="
 source /aws-foundry-ssl/setup/certbot.sh
